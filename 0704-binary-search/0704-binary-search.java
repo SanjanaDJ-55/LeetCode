68a691593a1090fdff8a9bb46b4fc66158ce00f6
SanjanaDJ-55 <sanjanadj371@gmail.com>
2026-07-11 10:16:47 +0530

    Time: 0 ms (100%), Space: 48.2 MB (68.98%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,11 +1,19 @@
 class Solution {
     public int search(int[] nums, int target) {
-        for(int i =0;i<=nums.length-1;i++){
-            if(nums[i]==target){
-                return i;
+        int l=0;
+        int r = nums.length-1;
+        while(l<=r){
+            int mid = l+((r-l)/2);
+            if(target == nums[mid]){
+                return mid;
+            }
+            else if(target>nums[mid]){
+                l =mid + 1;
+            }
+            else{
+                r = mid-1;
             }
         }
         return -1;
-        
     }
 }
