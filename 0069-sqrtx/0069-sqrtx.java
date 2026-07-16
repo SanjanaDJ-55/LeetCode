40383d18516e70526a0f19a0fa62d68bf9164ce9
SanjanaDJ-55 <sanjanadj371@gmail.com>
2026-07-16 17:41:41 +0530

    Time: 1 ms (97.61%), Space: 42.5 MB (78.99%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -4,18 +4,18 @@ class Solution {
             return 0;
         }
         int l =1;
-        int r = x;
+        int r =x;
         while(l<=r){
             int mid = l+(r-l)/2;
             long sqrt = (long)mid*mid;
-            if(sqrt==x){
+            if(sqrt == x){
                 return mid;
             }
-            else if(x<sqrt){
-                r = mid-1;
+            else if(x>sqrt){
+                l = mid+1;
             }
             else {
-                l = mid+1;
+                r = mid-1;
             }
         }
         return r;
