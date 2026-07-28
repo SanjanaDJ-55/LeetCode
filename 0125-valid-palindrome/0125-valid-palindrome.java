449942ab7074f21f967aebd9c9a55d0dcc7eb76b
SanjanaDJ-55 <sanjanadj371@gmail.com>
2026-07-28 23:05:19 +0530

    Time: 2 ms (99.32%), Space: 43.9 MB (98.4%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,19 +1,20 @@
 class Solution {
     public boolean isPalindrome(String s) {
+
         int left = 0;
         int right = s.length() - 1;
 
         while (left < right) {
-            // skip non-alphanumeric
+
             while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
                 left++;
             }
+
             while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
                 right--;
             }
 
-            // compare
-            if (Character.toLowerCase(s.charAt(left)) != 
+            if (Character.toLowerCase(s.charAt(left)) !=
                 Character.toLowerCase(s.charAt(right))) {
                 return false;
             }
@@ -21,6 +22,7 @@ class Solution {
             left++;
             right--;
         }
+
         return true;
     }
 }
