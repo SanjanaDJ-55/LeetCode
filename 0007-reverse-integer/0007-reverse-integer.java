9000bea8ea0101d8d7d84213d2c966311a1c9f44
SanjanaDJ-55 <sanjanadj371@gmail.com>
2026-08-30 13:18:39 +0530

    Time: 1 ms (99.98%), Space: 42.8 MB (11.3%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -4,7 +4,7 @@ class Solution {
        int rev = 0;
        while(x!=0){
        int rem = x%10;
-        x/=10;
+      
        if(rev >Integer.MAX_VALUE/10 || rev == Integer.MAX_VALUE/10      && rem> 7 ){
         return 0;
        }
@@ -13,6 +13,7 @@ class Solution {
         return 0;
        }
        rev = rev*10+rem;
+         x/=10;
       
        }
        return rev;
